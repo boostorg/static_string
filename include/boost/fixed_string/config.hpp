406b929fc56912a873fd2556e1ded32f9184af05
Vinnie Falco <vinnie.falco@gmail.com>
2019-09-09 13:56:14 -0700

    Initial commit

---
+++ include/boost/fixed_string/config.hpp
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/boostorg/fixed_string
+//
+
+#ifndef BOOST_FIXED_STRING_CONFIG_HPP
+#define BOOST_FIXED_STRING_CONFIG_HPP
+
+#include <boost/utility/string_view.hpp>
+
+namespace boost {
+namespace fixed_string {
+
+/// The type of `string_view` used by the library
+using string_view = boost::string_view;
+
+/// The type of `basic_string_view` used by the library
+template<class CharT, class Traits>
+using basic_string_view =
+    boost::basic_string_view<CharT, Traits>;
+
+} // fixed_string
+} // boost
+
+#endif
