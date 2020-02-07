2d719fff452a74a6dc5c0c76d59e331dc77f0271
Krystian Stasiowski <sdkrystian@gmail.com>
2020-02-07 14:49:47 -0500

    Update tests

--- test/static_string.cpp
+++ test/static_string.cpp
@@ -6961,31 +6961,14 @@ runTests()
 }
 
 static_assert(std::is_base_of<
-    detail::static_string_base_zero<0, char, std::char_traits<char>>, 
+    detail::static_string_base<0, char, std::char_traits<char>>,
     static_string<0>>::value, 
     "the zero size optimization shall be used for N = 0");
 
-#ifdef BOOST_STATIC_STRING_USE_NULL_OPTIMIZATION
 static_assert(std::is_base_of<
-    detail::static_string_base_null<1, char, std::char_traits<char>>, 
-    static_string<1>>::value, 
-    "the null terminator optimization shall be used for N <= (std::numeric_limits<char>::max)()");
-
-static_assert(std::is_base_of<
-    detail::static_string_base_null<(std::numeric_limits<char>::max)(), char, std::char_traits<char>>,
-    static_string<(std::numeric_limits<char>::max)()>>::value,
-    "the null terminator optimization shall be used for N <= std::numeric_limits<char>::max()");
-
-static_assert(std::is_base_of<
-    detail::static_string_base_zero<(std::numeric_limits<char>::max)() + 1, char, std::char_traits<char>>,
-    static_string<(std::numeric_limits<char>::max)() + 1>>::value,
-    "the minimum size type optimization shall be used for N > std::numeric_limits<char>::max()");
-#else
-static_assert(std::is_base_of<
-    detail::static_string_base_zero<(std::numeric_limits<char>::max)() + 1, char, std::char_traits<char>>,
+    detail::static_string_base<(std::numeric_limits<char>::max)() + 1, char, std::char_traits<char>>,
     static_string<(std::numeric_limits<char>::max)() + 1>>::value,
     "the minimum size type optimization shall be used for N > 0");
-#endif
 
 } // static_string
 } // boost
