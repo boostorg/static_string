6bd7010a3865287149197ed2427cf5692026d04b
Krystian Stasiowski <sdkrystian@gmail.com>
2020-02-22 16:05:51 -0500

    Update to_static_string tests

--- test/static_string.cpp
+++ test/static_string.cpp
@@ -3708,27 +3708,33 @@ testToStaticString()
     // 99% of the time.
     {
       auto str = to_static_string(std::numeric_limits<float>::max());
-      BOOST_TEST(str.find('e') != static_string<0>::npos);
+      BOOST_TEST(str.find('e') != static_string<0>::npos || str.find('.') !=
+        static_string<0>::npos);
     }
     {
       auto str = to_static_string(std::numeric_limits<double>::max());
-      BOOST_TEST(str.find('e') != static_string<0>::npos);
+      BOOST_TEST(str.find('e') != static_string<0>::npos || str.find('.') !=
+        static_string<0>::npos);
     }
     {
       auto str = to_static_string(std::numeric_limits<long double>::max());
-      BOOST_TEST(str.find('e') != static_string<0>::npos);
+      BOOST_TEST(str.find('e') != static_string<0>::npos || str.find('.') !=
+        static_string<0>::npos);
     }
     {
       auto str = to_static_wstring(std::numeric_limits<float>::max());
-      BOOST_TEST(str.find('e') != static_string<0>::npos);
+      BOOST_TEST(str.find('e') != static_string<0>::npos || str.find('.') !=
+        static_string<0>::npos);
     }
     {
       auto str = to_static_wstring(std::numeric_limits<double>::max());
-      BOOST_TEST(str.find('e') != static_string<0>::npos);
+      BOOST_TEST(str.find('e') != static_string<0>::npos || str.find('.') !=
+        static_string<0>::npos);
     }
     {
       auto str = to_static_wstring(std::numeric_limits<long double>::max());
-      BOOST_TEST(str.find('e') != static_string<0>::npos);
+      BOOST_TEST(str.find('e') != static_string<0>::npos || str.find('.') !=
+        static_string<0>::npos);
     }
 }
 
