d6696921111252d0a6af0f3dfae3200595ea1c24
Krystian Stasiowski <sdkrystian@gmail.com>
2020-03-02 19:37:11 -0500

    Change when ptr_in_range uses comparison functions

--- include/boost/static_string/config.hpp
+++ include/boost/static_string/config.hpp
@@ -180,8 +180,7 @@ defined(__clang__) && \
 // objects at all in constant expresssions. In these
 // cases, we use whatever will make more constexpr work.
 #if defined(__clang__) && \
-((defined(__GLIBCXX__) &&__GLIBCXX__ >= 20180726L \
-&& __GLIBCXX__ <= 20191102L) || defined(_MSC_VER))
+(defined(__GLIBCXX__) || defined(_MSC_VER))
 #define BOOST_STATIC_STRING_NO_PTR_COMP_FUNCTIONS
 #endif
 
